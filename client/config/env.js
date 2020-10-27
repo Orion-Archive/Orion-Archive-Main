6721727c3e65df31cd1d2026b378ea9fdf969b8d
sophiapak <sophiakpak@gmail.com>
2020-10-27 13:30:51 -0700

    added environment variables for ios and android analytics keys
    Co-authored-by: Justin Dury-Agri
    <justin.dury.agri@gmail.com>
    Co-authored-by: Nicholas Shay
    <shay.nicholas@gmail.com>
    Co-authored-by: Jinseon Shin
    <jinseonshin@gmail.com>

--- client/config/env.js
+++ client/config/env.js
@@ -1,5 +1,14 @@
-import { PG_URI } from 'react-native-dotenv';
+import { Platform } from 'react-native';
+import {
+  PG_URI,
+  IOS_ANALYTICS_KEY,
+  ANDROID_ANALYTICS_KEY,
+} from 'react-native-dotenv';
 
 export default {
   PG_URI,
+  ANALYTIC_KEY: Platform.select({
+    ios: IOS_ANALYTICS_KEY,
+    android: ANDROID_ANALYTICS_KEY,
+  }),
 };
