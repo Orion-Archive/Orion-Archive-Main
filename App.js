db317b224dbffe8ae36bf0fe4ac5ec9e5bd6ec6c
Jinseon Shin <jinseonshin@gmail.com>
2020-10-27 18:28:58 -0400

    Add OAuth ID to app.json

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-// import LoginScreen from "./client/screens/LoginScreen";
+import LoginScreen from "./client/screens/LoginScreen";
 import HomeScreen from "./client/screens/HomeScreen";
 
 export default function App() {
