08615d0c96ad9669b761fdb457a1c205ea7711bd
Jinseon Shin <jinseonshin@gmail.com>
2020-10-27 18:11:24 -0400

    resolve merge conflicts

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import LoginScreen from './client/screens/LoginScreen';
-import HomeScreen from './client/screens/HomeScreen';
+import React from "react";
+import { StyleSheet } from "react-native";
+// import LoginScreen from "./client/screens/LoginScreen";
+import HomeScreen from "./client/screens/HomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
-  );
+	return <HomeScreen />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
