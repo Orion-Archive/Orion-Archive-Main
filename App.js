ac0b3bc4867ebcf10afb8bd19028e8b49c944650
sophiapak <sophiakpak@gmail.com>
2020-10-26 17:58:19 -0700

    updated debugging launch json
    Co-authored-by: Justin Dury-Agri
    <justin.dury.agri@gmail.com>
    Co-authored-by: Nicholas Shay
    <shay.nicholas@gmail.com>
    Co-authored-by: Jinseon Shin
    <jinseonshin@gmail.com>”

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to Orion Archives</Text>
       <StatusBar style="auto" />
     </View>
   );
