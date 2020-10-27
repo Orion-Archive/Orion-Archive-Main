59698c701123b56d5afa0425ee19acddb06ec1e3
sophiapak <sophiakpak@gmail.com>
2020-10-27 15:28:47 -0700

    removed unsed stylesheet from App.js
    Co-authored-by: Justin Dury-Agri
    <justin.dury.agri@gmail.com>
    Co-authored-by: Nicholas Shay
    <shay.nicholas@gmail.com>
    Co-authored-by: Jinseon Shin
    <jinseonshin@gmail.com>

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import LoginScreen from './client/screens/LoginScreen';
 import HomeScreen from './client/screens/HomeScreen';
+import WelcomeScreen from './client/screens/WelcomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <HomeScreen />
-    </View>
-  );
+  return <HomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
