c0ec68b9edbb2aac2c327ff906d770de02cffb93
sophiapak <sophiakpak@gmail.com>
2020-10-26 18:29:07 -0700

    psuedo-coded initial notes in App.js
    Co-authored-by: Justin Dury-Agri
    <justin.dury.agri@gmail.com>
    Co-authored-by: Nicholas Shay
    <shay.nicholas@gmail.com>
    Co-authored-by: Jinseon Shin
    <jinseonshin@gmail.com>

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+} from 'react-native';
 
 export default function App() {
+  // TEXT ONPRESS HANDLER EXAMPLE
+  const handlePress = () => {
+    console.log('Text Pressed!');
+  };
+
   return (
+    // VIEW: ACTS LIKE A DIV
+    // VIEW -> SAFEAREAVIEW: MAKES SURE THAT THE IPHONE TOP NOTCH DOESN'T COVER OUR CONTENT
     <View style={styles.container}>
-      <Text>Welcome to Orion Archives</Text>
+      <Text onPress={handlePress}>Welcome to Orion Archives</Text>
+      {/* TOUCHABLE TAGS OFFER DIFFERENT USER FEEDBACK WHEN INTERACTING WITH ELEMENTS */}
+      <TouchableHighlight onPress={() => console.log('Image Tapped!')}>
+        {/* Sourced Images from Repo Assets Folder */}
+        {/* <Image source={require('./assets/icon.png')} /> */}
+        {/* Sourced Images from the Interwebs: Width & Height Properties Required */}
+        <Image
+          source={{
+            width: 400,
+            height: 752,
+            uri:
+              'https://www.clipartkey.com/mpngs/m/117-1174303_orion-the-hunter-drawing-orion-drawings-illustration-orion.png',
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +41,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    // FLEX: WILL GROW VERTICALLY AND HORIZONTALLY TO TAKE UP FULL SCREEN
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
