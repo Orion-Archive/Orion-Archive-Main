8557efcb9fda40496d227963686ab603c54ca105
sophiapak <sophiakpak@gmail.com>
2020-10-28 10:58:13 -0700

    added psuedo code for modalcomponent structure
    Co-authored-by: Justin Dury-Agri
    <justin.dury.agri@gmail.com>
    Co-authored-by: Nicholas Shay
    <shay.nicholas@gmail.com>
    Co-authored-by: Jinseon Shin
    <jinseonshin@gmail.com>

--- client/components/ModalComponent.js
+++ client/components/ModalComponent.js
@@ -14,11 +14,31 @@ function ModalComponent(props) {
     <View>
       <Modal
         animationType="none"
-        visible={true} /*{ PASSING IN STATE WHICH WILL BE A BOOLEAN }*/
+        /* VISIBLE={ PASSING IN STATE VIA PROPS WHICH WILL BE A BOOLEAN } */
+        visible={true}
         transparent={false}
-      ></Modal>
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}></View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  centeredView: {},
+  modalView: {},
+});
+
 export default ModalComponent;
+
+/**
+ * INITIAL MODAL:
+ * 		A. TOP NAV
+ * 				1. ADD BUTTON
+ * 						A: SIGNED IN? NAVIGATES YOU TO DIFF SCREEN
+ * 						B: NOT SIGNED IN? NAVIGATES YOU TO THE LOGINSCREEN
+ * 				2. X CLOSING BUTTON
+ * 		B. SCROLLABLE EVENT LIST
+ */
