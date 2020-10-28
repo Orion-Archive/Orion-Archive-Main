4ce35d8df55901b7ef90aa9d4f6563234a77a8b4
Jinseon Shin <jinseonshin@gmail.com>
2020-10-27 20:16:32 -0400

    Add OAuth for Google Sign In in LoginScreen.js

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import LoginScreen from "./client/screens/LoginScreen";
 import HomeScreen from "./client/screens/HomeScreen";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-export default function App() {
-	return <HomeScreen />;
-}
+// export default function App() {
+// 	return <LoginScreen />;
+// }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+const MainNavigator = createSwitchNavigator({
+	Login: { screen: LoginScreen },
+	Atlas: { screen: HomeScreen },
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
